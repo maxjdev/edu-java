73f4781d573d476342df6244f1307bbfb584ffee
maxjdev <maxjramosdev@gmail.com>
2023-05-06 13:56:37 -0300

    exercices

---
+++ src/edu/java/exercises/NameAge.java
@@ -0,0 +1,25 @@
+package src.edu.java.exercises;
+import java.util.Scanner;
+/*
+Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+public class NameAge {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name;
+        int age;
+
+        do {
+            System.out.println("What is your name ?");
+            name = input.nextLine();
+            System.out.println("How old are you ?");
+            age = input.nextInt();
+            input.nextLine();
+        } while (!name.equals("0"));
+        input.close();
+
+    }
+}

fa5cf133bb5a346bb7f5388fe66c6ccd280fb270
maxjdev <maxjramosdev@gmail.com>
2023-05-06 15:22:06 -0300

    exercice-reverse-order

---
+++ src/edu/java/exercises/ReverseOrder.java
@@ -0,0 +1,23 @@
+package src.edu.java.exercises;
+/*
+* crie um vetor de 6 posições
+* e exiba na ordem reversa
+* */
+public class ReverseOrder {
+    public static void main(String[] args) {
+        int[] vector = {-2, -1, 0, 1, 2};
+        int i = 0;
+
+        System.out.print("Vector: ");
+        while (i <= vector.length -1) {
+            System.out.print(" | " + vector[i] + " | ");
+            i++;
+        }
+
+        System.out.print("\nInverse vector: ");
+        for (int i1 = vector.length -1; i1 >= 0; i1--) {
+            System.out.print(" | " + vector[i1] + " | ");
+        }
+
+    }
+}
